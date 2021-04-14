8640cbb202964fac6fc2e64b712c49f8e3c8b418
zirpin <zirpin@gmail.com>
2021-04-14 16:24:27 +0200

    routing sin guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExpertPageComponent } from './pages/expert-page/expert-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { TagPageComponent } from './pages/tag-page/tag-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', // http:localhost:4200/
+    pathMatch: 'full',
+    redirectTo: '/experts'
+  },
+  {
+    path: 'home', // http:localhost:4200/home
+    component: HomePageComponent,
+  },
+  {
+    path: 'login', // http:localhost:4200/login
+    component: LoginPageComponent
+  },
+  {
+    path: 'register', // http:localhost:4200/register
+    component: RegisterPageComponent
+  },
+  {
+    path: 'experts',  // http:localhost:4200/experts
+    component: ExpertPageComponent,
+  },
+  {
+    path: 'tags',  // http:localhost:4200/tags
+    component: TagPageComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
