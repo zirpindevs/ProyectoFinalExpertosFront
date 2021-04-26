dbf337fa41e2739afe52c01da808bfb45910fb56
zirpin <zirpin@gmail.com>
2021-04-26 14:13:20 +0200

    desbloqueado el logout

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-home-page',
@@ -7,15 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   logout(): void {
-    // this.authService.setLoggedIn(false);
-    // sessionStorage.removeItem('Token');
-    // sessionStorage.removeItem('Id');
-    // this.router.navigate(['/login']);
+    this.authService.setLoggedIn(false);
+    sessionStorage.removeItem('Token');
+    sessionStorage.removeItem('Id');
+    this.router.navigate(['/login']);
   }
 }
